187690a8225dc7891b496b4e2f8eccfe1316f780
Andrii <andrewchepeliuk@gmail.com>
2023-05-04 01:29:14 +0300

    Add project files.

---
+++ HEDGEHOGS_Test/Hedgehogs.cs
@@ -0,0 +1,106 @@
+﻿namespace HEDGEHOGS_Test;
+
+public class Hedgehogs
+{
+    public int[] ColorsCount { get; private set; } = new int[3];
+
+    public enum Colors
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public void SetHedgehogsOfSpecificColor(Colors color)
+    {
+        bool result;
+        int numberOfHedgehogs;
+        do
+        {
+            Console.WriteLine($"Please, enter numberof {color} hedgehogs: ");
+            
+            result = int.TryParse(Console.ReadLine(), out numberOfHedgehogs);
+        }
+        while (!result);
+
+        if (numberOfHedgehogs >= 0)
+        {
+            ColorsCount[(int)color] = numberOfHedgehogs;
+            Console.Clear();
+            Print();
+        }
+    }
+
+    public int GetMinNumbersOfMeetings(int neededHedgehogColor)
+    {
+        if (!IsHedgehogsCountCorrect(ColorsCount[(int)Colors.Red],
+                                    ColorsCount[(int)Colors.Green],
+                                    ColorsCount[(int)Colors.Blue]))
+        {
+            return -1;
+        }
+
+        int secondHedgehogColorCount = 0, thirdHedgehogColorCount = 0;
+
+        for (int i = 0; i < ColorsCount.Length; i++)
+        {
+            if (i == neededHedgehogColor) continue;
+
+            if (secondHedgehogColorCount == 0)
+            {
+                secondHedgehogColorCount = ColorsCount[i];
+            }
+            else
+            {
+                thirdHedgehogColorCount = ColorsCount[i];
+            }
+        }
+
+        if (Math.Abs(secondHedgehogColorCount - thirdHedgehogColorCount) % 3 == 0)
+        {
+            if (secondHedgehogColorCount >= thirdHedgehogColorCount)
+            {
+                return secondHedgehogColorCount;
+            }
+            else
+            {
+                return thirdHedgehogColorCount;
+            }
+        }
+
+        return -1;
+    }
+
+    private bool IsHedgehogsCountCorrect(int redHedgehogs, int greenHedgehogs, int blueHedgehogs)
+    {
+        if (((redHedgehogs == 0) & (greenHedgehogs == 0))
+            || ((greenHedgehogs == 0) & (blueHedgehogs == 0))
+            || ((blueHedgehogs == 0) & (redHedgehogs == 0)))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Print()
+    {
+        Console.Write("Number of");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(" Red ");
+        Console.ResetColor();
+        Console.WriteLine($"hedgehogs: {ColorsCount[(int)Colors.Red]}");
+
+        Console.Write("Number of");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(" Green ");
+        Console.ResetColor();
+        Console.WriteLine($"hedgehogs: {ColorsCount[(int)Colors.Green]}");
+
+        Console.Write("Number of");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(" Blue ");
+        Console.ResetColor();
+        Console.WriteLine($"hedgehogs: {ColorsCount[(int)Colors.Blue]}");
+    }
+}
---
+++ HEDGEHOGS_Test/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using HEDGEHOGS_Test;
+
+var hedgehogs = new Hedgehogs();
+
+hedgehogs.Print();
+
+hedgehogs.SetHedgehogsOfSpecificColor(Hedgehogs.Colors.Red);
+hedgehogs.SetHedgehogsOfSpecificColor(Hedgehogs.Colors.Green);
+hedgehogs.SetHedgehogsOfSpecificColor(Hedgehogs.Colors.Blue);
+
+bool result;
+int neededHedgehogColor;
+do
+{
+    Console.WriteLine("Please, select color!\n0 - Red\n1 - Green\n2 - Blue \nEnter number:");
+
+    result = int.TryParse(Console.ReadLine(), out neededHedgehogColor);
+
+    if (neededHedgehogColor < 0 || neededHedgehogColor > 2)
+    {
+        Console.Clear();
+        hedgehogs.Print();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You entered wrong number!");
+        Console.ResetColor();
+        result = false;
+    }
+}
+while (!result);
+
+var encountersCount = hedgehogs.GetMinNumbersOfMeetings(neededHedgehogColor);
+if (encountersCount >= 0)
+{
+    Console.WriteLine($"\nThe minimum number of encounters: {encountersCount}.");
+}
+else Console.WriteLine("It is impossible to change all hedgehogs to a given color!");
+
+Console.ReadKey();
